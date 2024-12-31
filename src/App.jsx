fa2c4c4939b44e3501793da622321999ce089d58
SanjayB2005 <bsanjay2595@gmail.com>
2024-12-31 20:14:45 +0530

    Removed comment line

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import ProductDetails, { ProductDetailsLoader } from './Pages/ProductDetails';
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
-  // APP COMPONENT
   const [cart, setCart] = useState(() => {
     const savedCart = localStorage.getItem('cart');
     return savedCart ? JSON.parse(savedCart) : [];
